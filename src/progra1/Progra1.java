c6746f7059cd12c58fd78e6ad731b5a5bd29bc8c
Victor Vasquez <victor.vasquez96@unitec.edu>
2015-04-24 08:55:07 -0500

    Agregar clases de fechas y hola mundo

--- src/progra1/Progra1.java
+++ src/progra1/Progra1.java
@@ -15,7 +15,7 @@ public class Progra1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola Mundo");
     }
     
 }
