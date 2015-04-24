d3fc8a4065953f3510502cd14a84b838babf048b
Victor Vasquez <victor.vasquez96@unitec.edu>
2015-04-24 08:47:43 -0500

    Test de clase

---
+++ src/progra1/Progra1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra1;
+
+/**
+ *
+ * @author Pavilion 14-N036
+ */
+public class Progra1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
